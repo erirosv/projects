8c2c4f2c0d275c21418c3249e43e5366add40c8e
Erik Rosvall <erirosv@gmail.com>
2023-12-28 13:51:51 +0100

    Adding a simple server/client as a POC to test with the pies

--- weather_nodes/poc/server.py
+++ weather_nodes/poc/server.py
@@ -7,19 +7,15 @@ latest_data = {}
 @app.route('/upload', methods=['POST'])
 def upload():
     data = request.get_json()
-    print("Received data:", data)
+    node_identifier = request.remote_addr  # Use the IP address of the sender as an identifier
 
-    # Get a unique identifier for the node, e.g., its IP address
-    node_identifier = request.remote_addr
-
-    # Store the latest data for each node
     latest_data[node_identifier] = data
-    
+    print(f"Received data from {node_identifier}: {data}")
+
     return 'Data received successfully'
 
 @app.route('/')
 def index():
-    # Render the HTML page with the latest data from each node
     return render_template('index.html', data=latest_data)
 
 if __name__ == '__main__':
