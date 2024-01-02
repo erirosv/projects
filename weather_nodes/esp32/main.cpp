98c002e1f899abe8e3315d63a0e3ecaa5cb16825
Erik Rosvall <erirosv@gmail.com>
2024-01-02 23:41:02 +0100

    first draft for the esp32 module

---
+++ weather_nodes/esp32/main.cpp
@@ -0,0 +1,51 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <WiFi.h>
+
+const char* ssid = "ASUS_AP";
+const char* password = "33jonsered";
+const char* serverIP = "192.168.50.12"; 
+const int serverPort = 5050;
+const char* location = "Living Room";  
+
+Adafruit_BME280 bme;
+
+void setup() {
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+
+    if (!bme.begin()) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+}
+
+void loop() {
+    // Read sensor data
+    float temperature = bme.readTemperature();
+    float pressure = bme.readPressure() / 100.0F;
+    float humidity = bme.readHumidity();
+
+    WiFiClient client;
+    if (client.connect(serverIP, serverPort)) {
+        String message = String(location) + ":" + String(temperature) + ":" + String(pressure) + ":" + String(humidity);
+        client.print(message.length());
+        delay(10);
+        client.print(message);
+        delay(10);
+        client.stop();
+        Serial.println("Data sent to server");
+    } else {
+        Serial.println("Failed to connect to server");
+    }
+
+    delay(10000); // delay for testing 10 sec
+    //delay(600000); // Dealy 10 min
+}
